96ccfd812ab4a385ad7f7c62e57d5fd78aabbd90
Devanshu Singh <parihardevanshusingh@gmail.com>
2026-02-24 14:05:39 +0530

    Delete portfoliowebsite directory

--- portfoliowebsite/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 50) {
-        document.querySelector('.navbar').classList.add('bg-opacity-90');
-    } else {
-        document.querySelector('.navbar').classList.remove('bg-opacity-90');
-    }
-});
